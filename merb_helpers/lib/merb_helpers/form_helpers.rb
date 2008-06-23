140ec318db2d509682986a0075639fb3a3ad73d4
Yehuda Katz <wycats@gmail.com>
2008-06-23 02:39:25 -0700

    Wrap the setup in an after_app_loads so that the form_class in question will already have the Form Builder constants.

--- merb_helpers/lib/merb_helpers/form_helpers.rb
+++ merb_helpers/lib/merb_helpers/form_helpers.rb
@@ -2,12 +2,15 @@ load File.dirname(__FILE__) / "tag_helpers.rb"
 load File.dirname(__FILE__) / "form" / "helpers.rb"
 load File.dirname(__FILE__) / "form" / "form.rb"
 
-Merb::Plugins.config[:helpers] ||= {}
-Merb::Plugins.config[:helpers][:form_class] ||= Merb::Form
+Merb::BootLoader.after_app_loads do
 
-class Merb::Controller
-  class_inheritable_accessor :_form_class
-  self._form_class = Merb::Plugins.config[:helpers][:form_class]
+  Merb::Plugins.config[:helpers][:form_class] ||= Merb::Form
+
+  class Merb::Controller
+    class_inheritable_accessor :_form_class
+    self._form_class = Merb::Plugins.config[:helpers][:form_class]
   
-  include Merb::Helpers::Form
+    include Merb::Helpers::Form
+  end
+
 end
