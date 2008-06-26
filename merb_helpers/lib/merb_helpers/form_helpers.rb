96d5bca90b178e68bc61a50bdf0768706309d633
Yehuda Katz <wycats@gmail.com>
2008-06-26 16:20:23 -0700

    try and resolve the issues with load order

--- merb_helpers/lib/merb_helpers/form_helpers.rb
+++ merb_helpers/lib/merb_helpers/form_helpers.rb
@@ -2,15 +2,17 @@ load File.dirname(__FILE__) / "tag_helpers.rb"
 load File.dirname(__FILE__) / "form" / "helpers.rb"
 load File.dirname(__FILE__) / "form" / "form.rb"
 
+class Merb::Controller
+  class_inheritable_accessor :_form_class
+  include Merb::Helpers::Form  
+end
+
 Merb::BootLoader.after_app_loads do
 
   Merb::Plugins.config[:helpers][:form_class] ||= Merb::Form
 
   class Merb::Controller
-    class_inheritable_accessor :_form_class
     self._form_class = Merb::Plugins.config[:helpers][:form_class]
-  
-    include Merb::Helpers::Form
   end
 
 end
